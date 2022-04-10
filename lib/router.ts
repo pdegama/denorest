076b5672b7c1b4ba3df3db82c23b1c346b5d27e5
elparthka <parthka.2005@gmail.com>
2022-04-10 16:04:41 +0530

    add router system

--- lib/router.ts
+++ lib/router.ts
@@ -1,20 +1,32 @@
+import path_parse from "./path_parse.ts";
+
 class Router {
 
-    public paths:any = {};
+    public paths: any = {};
+    public routes: any = [];
+    public hands: any = [];
 
     public set = async (path: string, hand: Function) => {
         this.paths[path] = hand;
     }
 
     public get = async () => {
+
         for (const k in this.paths) {
-            console.log(k)
+            this.routes.push(path_parse(k));
+            this.hands.push(this.paths[k]);
         }
+
+        return {
+            routes: this.routes,
+            hands: this.hands
+        }
+
     }
 
-    public pre = async (path:string, route: Router) => {
+    public pre = async (path: string, route: Router) => {
         for (const pathKey in route.paths) {
-            this.paths[path+pathKey] = route.paths[pathKey];
+            this.paths[path + (pathKey !== "/" ? pathKey : "")] = route.paths[pathKey];
         }
     }
 
