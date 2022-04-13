6ae2977b33dd79c6934faa30f8f1317218cb20b2
elparthka <parthka.2005@gmail.com>
2022-04-13 17:53:31 +0530

    reqParse -> bodyParse

--- mod.ts
+++ mod.ts
@@ -1,9 +1,9 @@
 import Application from './lib/server.ts';
 import Router from "./lib/router.ts";
-import reqParse from './lib/body_parse.ts'
+import bodyParse from './lib/body_parse.ts'
 
 export {
     Application,
     Router,
-    reqParse
+    bodyParse
 }
