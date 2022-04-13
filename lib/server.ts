b32505fe8e48fa57201e0d80dacff3f3f1e82210
elparthka <parthka.2005@gmail.com>
2022-04-13 17:48:23 +0530

    remove body parser

--- lib/server.ts
+++ lib/server.ts
@@ -1,18 +1,17 @@
-import { serve } from "https://deno.land/std/http/server.ts";
+import {serve} from "https://deno.land/std/http/server.ts";
 import Router from "./router.ts";
-import body_parse from "./body_parse.ts";
 
 export default class {
 
-    private port:number = 8080;
-    private hand404:any;
-    public routes:any = {};
+    private port: number = 8080;
+    private hand404: any;
+    public routes: any = {};
 
-    constructor(port:number = 8000){
+    constructor(port: number = 8000) {
         this.port = port;
     }
 
-    public set = async (r:Router) => {
+    public set = async (r: Router) => {
         this.routes = await r.getRoutes();
         this.hand404 = await this.routes[this.routes.length - 1].hand;
     }
@@ -23,43 +22,40 @@ export default class {
         const url = new URL(req.url);
 
         let is404 = true;
-        let body = "";
 
-        let res:any = {
+        let res: any = {
             reply: "",
             headers: {},
             status: 200
         }
 
-        let r:any = {}
+        let r: any = {}
 
         for (const ele of this.routes) {
-            if (url.pathname.match(ele.path) && (ele.method === req.method || ele.method === 'ALL')){
+            if (url.pathname.match(ele.path) && (ele.method === req.method || ele.method === 'ALL')) {
                 is404 = false;
                 if (req.body) {
-                    body = await body_parse(req);
+                    r.body = await req.text();
                 }
                 r.headers = req.headers;
                 r.method = req.method;
                 r.url = req.url;
-                r.body = body;
                 await ele.hand(r, res);
                 break;
             }
         }
 
-        if(is404){
+        if (is404) {
             if (req.body) {
-                body = await body_parse(req);
+                r.body = await req.text();
             }
             r.headers = req.headers;
             r.method = req.method;
             r.url = req.url;
-            r.body = body;
             await this.hand404(r, res);
         }
 
-        return new Response(typeof res.reply === "object" ? JSON.stringify(res.reply) : res.reply,{
+        return new Response(typeof res.reply === "object" ? JSON.stringify(res.reply) : res.reply, {
             status: res.status,
             headers: res.headers
         });
