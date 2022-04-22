d24b4ec17c3abcf69116a97ac45340685ae1005d
elparthka <parthka.2005@gmail.com>
2022-04-22 20:00:07 +0530

    remove vercel file

--- api/oak_server.ts
+++
@@ -1,9 +0,0 @@
-import { Application } from "https://deno.land/x/oak/mod.ts";
-
-const app = new Application();
-
-app.use((ctx) => {
-  ctx.response.body = "Hello, Oak!";
-});
-
-await app.listen({ port: 8001 });
