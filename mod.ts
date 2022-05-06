09686488eb0f3e5fd33216df4ee1c0dbd6f41d47
Parth Degama <parthka.2005@gmail.com>
2022-05-06 15:15:35 +0530

    Application to WebApp

--- mod.ts
+++ mod.ts
@@ -1,7 +1,7 @@
-import Application from "./lib/server.ts";
+import WenApp from "./lib/server.ts";
 import Router from "./lib/router.ts";
 import bodyParse from "./lib/body_parse.ts";
 import { Req, Res } from "./lib/types.ts";
 
 export type { Req, Res };
-export { Application, bodyParse, Router };
+export { WenApp, bodyParse, Router };
