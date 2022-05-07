321f8f27a64a4e41cb7d9c8e7a170cc7820c6662
Parth Degama <parthka.2005@gmail.com>
2022-05-07 22:07:16 +0530

    change module name

--- mod.ts
+++ mod.ts
@@ -1,8 +1,8 @@
 import WenApp from "./lib/server.ts";
 import Router from "./lib/router.ts";
 import bodyParse from "./lib/util/body_parse.ts";
-import urlParser from "./lib/util/url_parse.ts";
+import pathParse from "./lib/util/path_parse.ts";
 import { Req, Res } from "./lib/types.ts";
 
 export type { Req, Res };
-export { bodyParse, Router, urlParser, WenApp };
+export { bodyParse, pathParse, Router, WenApp };
