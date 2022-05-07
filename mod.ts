15824dc09111741956579214cab3e7c93af108d3
Parth Degama <parthka.2005@gmail.com>
2022-05-07 20:38:38 +0530

    add parser link

--- mod.ts
+++ mod.ts
@@ -1,7 +1,8 @@
 import WenApp from "./lib/server.ts";
 import Router from "./lib/router.ts";
 import bodyParse from "./lib/util/body_parse.ts";
+import urlParser from "./lib/util/url_parse.ts";
 import { Req, Res } from "./lib/types.ts";
 
 export type { Req, Res };
-export { bodyParse, Router, WenApp };
+export { bodyParse, Router, urlParser, WenApp };
